4ce061fcdc7d5131ae5ff2bb7d40d3a8677442ec
Himanshu K.C <kc9himanshu@gmail.com>
2024-04-17 16:34:46 +1000

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -4,12 +4,13 @@ window.addEventListener('DOMContentLoaded' , (event) => {
 
 
 //URL for the local backend API (for testing)
-const functionApi = 'http://localhost:7071/api/ResumeChallengeCounter';
+const functionApiURL = 'https://azureresumefunction.azurewebsites.net/api/ResumeChallengeCounter?code=3M0ZFOKPI3x1HigOxnrygUYWgTrLYy_vXyXiXQzhXQ5UAzFuz8zkeA=='
+const localfunctionApi = 'http://localhost:7071/api/ResumeChallengeCounter';
 
 //This function fetches the visit count from the backend API and displays it on the webpage
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiURL).then(response => {
         return response.json()
     }).then (response =>{
         console.log ("Website called function API.");
